a6a009af130f5ed3954faf5f9b8741e2c388c153
Gia Phong <ngogiaphong123@gmail.com>
2023-07-14 08:44:07 +0700

    Complete quizzes page with API call

--- client/src/routes/dashboard/quizzes/+page.svelte
+++ client/src/routes/dashboard/quizzes/+page.svelte
@@ -3,7 +3,28 @@
 	import SearchSort from '../../../components/searchSort.svelte';
 	export let data;
 	let { quizzes } = data;
+	console.log(quizzes);
 </script>
 
-<SearchSort />
-<Quizzes {quizzes} />
+{#if quizzes.length !== 0}
+	<SearchSort />
+	<Quizzes {quizzes} />
+{:else}
+	<div class="" />
+	<div class="flex items-center h-full">
+		<div
+			class="container flex flex-col md:flex-row sm:flex-row items-center justify-center px-5 text-gray-700"
+		>
+			<div class="w-full lg:w-1/2 md:w-1/3 sm:w-1/3 mx-8">
+				<p class="text-2xl md:text-3xl sm:text-2xl font-light leading-normal mb-8">
+					No quizzes found
+				</p>
+				<button
+					aria-label="Create quizzes"
+					class="block p-4 py-2 rounded-md bg-secondary hover:bg-darkGreen text-white focus:outline-none"
+					>Create quizzes</button
+				>
+			</div>
+		</div>
+	</div>
+{/if}
