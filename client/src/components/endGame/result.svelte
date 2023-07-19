cccfa8a5180f66695e0a6934cbf1d52a1bc59e98
thanhtinh06 <nguyenthithanhtinh06@gmail.com>
2023-07-19 10:24:50 +0700

    responsive

--- client/src/components/endGame/result.svelte
+++ client/src/components/endGame/result.svelte
@@ -25,11 +25,11 @@
 </script>
 
 <div class="flex items-center justify-center">
-	<div class="grid grid-cols-2 bg-white shadow-lg w-96 p-6 rounded-xl gap-6">
+	<div class="grid grid-cols-2 bg-white shadow-lg md:w-96 max-w-md p-6 rounded-xl md:gap-6 gap-4">
 		{#each elementsResult as { name, value, color }}
 			<div class="flex gap-2">
 				<Icon icon={'icon-park-outline:dot'} {color} class={'text-2xl'} />
-				<div class="flex flex-col items-start text-lg">
+				<div class="flex flex-col items-start md:text-lg text-base">
 					<p class="text-{color} font-semibold">{value}</p>
 					<p>{name}</p>
 				</div>
