24f1390493ddab2596eeaa59bba3a9a87167b18c
thanhtinh06 <nguyenthithanhtinh06@gmail.com>
2023-07-19 10:07:24 +0700

    add historyAttempt

--- client/src/components/endGame/historyAttempt.svelte
+++ client/src/components/endGame/historyAttempt.svelte
@@ -1,3 +1,25 @@
-<div>
-	<div />
+<div class="w-full">
+	<div class="flex justify-center overflow-x-auto">
+		<table class="w-1/2 text-left text-gray-500 dark:text-gray-400">
+			<thead class="uppercase bg-darkGreen text-white">
+				<tr>
+					<th scope="col" class="px-6 py-3"> No. </th>
+					<th scope="col" class="px-6 py-3"> Points </th>
+					<th scope="col" class="px-6 py-3"> Status </th>
+				</tr>
+			</thead>
+			<tbody class="text-gray-900">
+				<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+					<th scope="row" class="px-6 py-4 font-medium whitespace-nowrap"> Attempt 1 </th>
+					<td class="px-6 py-4"> 10 </td>
+					<td class="px-6 py-4">Fail </td>
+				</tr>
+				<tr class="bg-white border-b">
+					<th scope="row" class="px-6 py-4 font-medium whitespace-nowrap"> Attempt 2 </th>
+					<td class="px-6 py-4"> 100 </td>
+					<td class="px-6 py-4">Pass </td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
