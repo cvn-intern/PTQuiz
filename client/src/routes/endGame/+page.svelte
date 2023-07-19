b62cc9bfcde694beb79968622817467785133775
thanhtinh06 <nguyenthithanhtinh06@gmail.com>
2023-07-19 10:57:39 +0700

    fix responsive

--- client/src/routes/endGame/+page.svelte
+++ client/src/routes/endGame/+page.svelte
@@ -1,11 +1,11 @@
-<script>
+<script lang="ts">
 	import HistoryAttempt from '../../components/endGame/historyAttempt.svelte';
 	import Result from '../../components/endGame/result.svelte';
 	import Score from '../../components/endGame/score.svelte';
 </script>
 
-<div class="bg-greenLight h-screen">
-	<div class="flex flex-col gap-10 items-center pt-6">
+<div class="bg-greenLight w-full">
+	<div class="flex flex-col gap-10 items-center py-6">
 		<Score />
 		<Result />
 		<HistoryAttempt />
