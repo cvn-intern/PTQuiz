2eeefcdd8e1277cd0f89b764c32f003c19ea1056
thanhtinh06 <nguyenthithanhtinh06@gmail.com>
2023-07-19 10:13:55 +0700

    change HistoryAttempt

--- client/src/components/endGame/historyAttempt.svelte
+++ client/src/components/endGame/historyAttempt.svelte
@@ -1,3 +1,18 @@
+<script>
+	const AttemptList = [
+		{
+			Attempt: 1,
+			Points: 10,
+			Status: 'Fail'
+		},
+		{
+			Attempt: 2,
+			Points: 100,
+			Status: 'Pass'
+		}
+	];
+</script>
+
 <div class="w-full">
 	<div class="flex justify-center overflow-x-auto">
 		<table class="w-1/2 text-left text-gray-500 dark:text-gray-400">
@@ -9,16 +24,15 @@
 				</tr>
 			</thead>
 			<tbody class="text-gray-900">
-				<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-					<th scope="row" class="px-6 py-4 font-medium whitespace-nowrap"> Attempt 1 </th>
-					<td class="px-6 py-4"> 10 </td>
-					<td class="px-6 py-4">Fail </td>
-				</tr>
-				<tr class="bg-white border-b">
-					<th scope="row" class="px-6 py-4 font-medium whitespace-nowrap"> Attempt 2 </th>
-					<td class="px-6 py-4"> 100 </td>
-					<td class="px-6 py-4">Pass </td>
-				</tr>
+				{#each AttemptList as { Attempt, Points, Status }}
+					<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+						<th scope="row" class="px-6 py-4 font-medium whitespace-nowrap">
+							{Attempt}
+						</th>
+						<td class="px-6 py-4"> {Points} </td>
+						<td class="px-6 py-4"> {Status} </td>
+					</tr>
+				{/each}
 			</tbody>
 		</table>
 	</div>
