67a5a6c49d9ac3909046879cfafc0328c8f8a25d
thanhtinh06 <nguyenthithanhtinh06@gmail.com>
2023-07-20 17:59:58 +0700

    fix UIplaygame

--- client/src/routes/playGame/[quizzesId]/+page.svelte
+++ client/src/routes/playGame/[quizzesId]/+page.svelte
@@ -2,20 +2,48 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { gameInfoStore } from '../../../libs/store/gameInfoStore';
+	import logo from '../../../assets/logo.png';
+	import clsx from 'clsx';
 	export let data;
 
 	let gameInfo = data.gameInfo;
 	gameInfoStore.update((val) => (val = gameInfo));
 	let quizzesId = $page.params.quizzesId;
+	$: textReady = 'ARE';
+	setTimeout(() => {
+		textReady = 'YOU';
+	}, 500);
+	setTimeout(() => {
+		textReady = 'READY ?';
+	}, 1000);
+	$: isShowButton = false;
+	setTimeout(() => {
+		isShowButton = true;
+	}, 1500);
 </script>
 
-<div class="flex w-full justify-center items-center">
-	<button
-		class="p-6 bg-secondary rounded-full text-4xl hover:bg-buttonHover"
-		on:click={() => {
-			goto(`/playGame/${quizzesId}/play`);
-		}}
-	>
-		Play Game
-	</button>
+<div class="w-full bg-greenLight">
+	<div class="flex flex-col justify-center items-center gap-6 py-20 md:py-0 h-full">
+		<div class="h-40">
+			<img src={logo} alt="logo" class="md:w-52 w-28 animate-wiggle" />
+		</div>
+		<div class="md:text-9xl text-6xl font-bold text-white">
+			<h1 class="ease-in-out transition-all duration-100">{textReady}</h1>
+		</div>
+		<div>
+			<button
+				class={clsx(
+					' text-white font-bold justify-center transition duration-200 ease-in-out transform px-4 py-2 w-48 border-b-4 border-zinc-500 hover:border-b-2 bg-secondary rounded-2xl hover:translate-y-px',
+					{
+						hidden: !isShowButton
+					}
+				)}
+				on:click={() => {
+					goto(`/playGame/${quizzesId}/play`);
+				}}
+			>
+				Play Game
+			</button>
+		</div>
+	</div>
 </div>
