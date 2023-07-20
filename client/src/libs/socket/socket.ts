d95349b89bfb19658937e9d1092b0c41379b33d4
Ngo Gia Phong <ngogiaphong123@gmail.com>
2023-07-20 20:18:54 +0700

    Deploy

--- client/src/libs/socket/socket.ts
+++ client/src/libs/socket/socket.ts
@@ -2,7 +2,8 @@ import { io } from 'socket.io-client';
 
 const socket = io(`${import.meta.env.VITE_SOCKET_URL}`, {
 	transports: ['websocket'],
-	port: 443
+	port: 443,
+    secure: true,
 });
 
 export default socket;
