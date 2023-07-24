ecb5da01e726209a4a6dc951dd0578cb49f9f4d2
hvdhavietduc <duc.ha000412@hcmut.edu.vn>
2023-07-24 16:54:29 +0700

    screen for create question

---
+++ client/src/components/createQuestion/createQuestion.svelte
@@ -0,0 +1,31 @@
+<div class="flex flex-row h-[800] w-full">
+	<div class=" h-full w-1/4" />
+	<div class="h-full w-3/4 flex flex-col p-6">
+		<div class=" h-1/3 w-full flex flex-col items-center">
+			<input type="text" class=" w-3/4 p-5 m-1" /><br />
+			<input type="file" name="" id="" />
+		</div>
+		<div class=" grid grid-cols-2 gap-3 h-2/3 w-full">
+			<div class=" bg-optionA rounded-xl flex flex-row items-center ">
+				<h1 class=" mx-4 text-4xl font-semibold">A.</h1>
+				<textarea type="text" class=" text-center resize-none h-2/3 w-3/4 bg-optionA border-none border-transparent focus:border-transparent focus:ring-0 text-2xl" />
+                <input type="radio" name="trueAnswer" id="" class=" p-5 ml-5">
+			</div>
+			<div class=" bg-optionB rounded-xl flex flex-row items-center justify-center">
+				<h1 class="w-1/5 text-4xl ml-4 font-semibold">B.</h1>
+				<textarea type="text" class=" resize-none h-1/2 w-1/2 bg-optionB border-none border-transparent focus:border-transparent focus:ring-0 text-2xl" />
+                <input type="radio" name="trueAnswer" id="" class=" p-5 ml-5">
+			</div>
+            <div class=" bg-optionC rounded-xl flex flex-row items-center justify-center">
+				<h1 class="w-1/5 text-4xl ml-4 font-semibold">C.</h1>
+				<textarea type="text" class="resize-none h-1/2 w-1/2 bg-optionC border-none border-transparent focus:border-transparent focus:ring-0 text-2xl" />
+                <input type="radio" name="trueAnswer" id="" class=" p-5 ml-5">
+			</div>
+            <div class=" bg-optionD rounded-xl flex flex-row items-center justify-center">
+				<h1 class="w-1/5 text-4xl ml-4 font-semibold">D.</h1>
+				<textarea type="text" class="resize-none h-1/2 w-1/2 bg-optionD border-none border-transparent focus:border-transparent focus:ring-0 text-2xl" />
+                <input type="radio" name="trueAnswer" id="" class=" p-5 ml-5">
+			</div>
+		</div>
+	</div>
+</div>
