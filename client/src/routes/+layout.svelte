c46f737ad7e8543fe661d6108ac2f42328bea111
ngogiaphong123 <ngogiaphong123@gmail.com>
2023-07-24 16:35:42 +0700

    remove js-cookie

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -4,12 +4,7 @@
 	import Footer from '../components/layout/footer.svelte';
 	import { Toaster } from 'svelte-french-toast';
 	import type { LayoutData } from './$types';
-	import Cookies from 'js-cookie';
-	import { locale, t, locales } from '$i18n/translations';
 
-	const handleChange = ({ currentTarget }) => {
-		Cookies.set('lang', currentTarget.value);
-	};
 	export let data: LayoutData;
 </script>
 
@@ -18,9 +13,4 @@
 <main class="flex flex-1 bg-background">
 	<slot />
 </main>
-<select bind:value={$locale} on:change={handleChange}>
-	{#each $locales as value}
-		<option {value}>{$t(`lang.${value}`)}</option>
-	{/each}
-</select>
 <Footer />
