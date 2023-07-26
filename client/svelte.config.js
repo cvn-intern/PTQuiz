a9e7e55d9b97a3fca536e072e2cc8161df3dfd5c
ngogiaphong123 <ngogiaphong123@gmail.com>
2023-07-26 12:30:26 +0700

    Svelte.config reformat

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -3,27 +3,27 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter({
             precompress: true,
         }),
-		alias: {
-			$components: 'src/components',
-			$assets: 'src/assets',
-			$actions: 'src/libs/actions',
-			$helpers: 'src/libs/helpers',
-			$stores: 'src/libs/stores',
-			$i18n: 'src/libs/i18n',
-			$types: 'src/app.d.ts',
-			$constants: 'src/libs/constants'
-		}
-	},
-	preprocess: vitePreprocess()
+        alias: {
+            $components: 'src/components',
+            $assets: 'src/assets',
+            $actions: 'src/libs/actions',
+            $helpers: 'src/libs/helpers',
+            $stores: 'src/libs/stores',
+            $i18n: 'src/libs/i18n',
+            $types: 'src/app.d.ts',
+            $constants: 'src/libs/constants'
+        }
+    },
+    preprocess: vitePreprocess()
 };
 
 export default config;
