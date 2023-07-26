f8c6d94615e099cf88bfe1f237565874425f6ded
Pham Quoc Cuong <pzcuong@Pham-Quoc-Cuong.local>
2023-07-26 20:11:31 +0700

    add get for question

---
+++ client/src/routes/api/question/get/[questionId]/+server.ts
@@ -0,0 +1,16 @@
+import { error, json, type RequestHandler } from '@sveltejs/kit';
+import { VITE_API_URL } from '$env/static/private';
+import { HttpStatus } from '$constants/httpStatus';
+
+export const GET: RequestHandler = async ({ fetch, params }) => {
+	const response = await fetch(`${VITE_API_URL}/question/get?questionId=${params.questionId}`, {
+		method: 'GET'
+	});
+
+	const result = await response.json();
+
+	if (result.statusCode !== HttpStatus.OK) {
+		throw error(HttpStatus.BAD_REQUEST, result.message);
+	}
+	return json(result);
+};
