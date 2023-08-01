cb823220692b1a316dcc4b42e41b60c6578505c7
thanhtinh06 <nguyenthithanhtinh06@gmail.com>
2023-08-01 17:01:46 +0700

    open Modal

--- client/src/components/quizzes/quiz.svelte
+++ client/src/components/quizzes/quiz.svelte
@@ -3,6 +3,7 @@
 	import toast, { Toaster } from 'svelte-french-toast';
 	import { t } from '../../libs/i18n/translations';
 	import Icon from '@iconify/svelte';
+	import DetailQuiz from '$components/detailQuiz/detailQuiz.svelte';
 
 	export let title: string;
 	export let author: string;
@@ -53,7 +54,7 @@
 		goto(`/createQuiz/${id}`);
 	}
 
-	async function deleteQuiz(id:string) {
+	async function deleteQuiz(id: string) {
 		const response = await fetch(`/api/quizzes/delete/${id}`, {
 			method: 'DELETE'
 		});
@@ -65,12 +66,13 @@
 			toast.error(result.message);
 		}
 	}
-
+	$: isOpen = false;
 </script>
 
-<section
+<button
 	class="flex flex-row justify-center md:items-start items-center border-rose-50 gap-3 border-solid shadow-md p-6 hover:shadow-md transition duration-200 transform hover:scale-102 rounded-xl cursor-pointer"
 	aria-details="Quiz Details"
+	on:click={() => (isOpen = !isOpen)}
 >
 	<div>
 		<img class="w-[176px] h-32 rounded-xl" src={image} alt={title} />
@@ -81,12 +83,10 @@
 				<h1 class="md:text-2xl text-base font-bold max-w-sm whitespace-pre-wrap">
 					{title}
 				</h1>
-				<button on:click={()=>deleteQuiz(id)}>
+				<button on:click={() => deleteQuiz(id)}>
 					<Icon icon="iconamoon:trash-fill" class="text-2xl text-red-600" />
 				</button>
 			</div>
-
-			<p class="text-sm text-zinc-400 md:w-full md:block hidden">{description}</p>
 		</div>
 		<div
 			class="flex md:flex-row flex-col md:justify-between md:items-center items-start md:gap-4 gap-2"
@@ -111,4 +111,5 @@
 			</div>
 		</div>
 	</div>
-</section>
+</button>
+<DetailQuiz {id} {isOpen} />
