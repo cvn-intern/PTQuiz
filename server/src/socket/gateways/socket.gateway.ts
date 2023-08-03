4a6d5b1218b8be5479c657fa3c8e04cd8f96d39c
ngogiaphong123 <ngogiaphong123@gmail.com>
2023-08-03 16:19:04 +0700

    Uncomment start,end game gateway

--- server/src/socket/gateways/socket.gateway.ts
+++ server/src/socket/gateways/socket.gateway.ts
@@ -175,7 +175,7 @@ export class SocketGateway
     ) {
         try {
             const { roomPIN } = data;
-            // await this.socketService.startGame(roomPIN, client.user.id);
+            await this.socketService.startGame(roomPIN, client.user.id);
             this.server.to(roomPIN).emit(EmitChannel.STARTED, {
                 isStarted: true,
             });
@@ -193,7 +193,7 @@ export class SocketGateway
     ) {
         try {
             const { roomPIN } = data;
-            // await this.socketService.endGame(roomPIN, client.user.id);
+            await this.socketService.endGame(roomPIN, client.user.id);
             this.server.to(roomPIN).emit('ended', {
                 isEnded: true,
             });
