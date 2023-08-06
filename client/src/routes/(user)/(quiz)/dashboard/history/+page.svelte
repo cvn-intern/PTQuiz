f5d93d44f0094306b24795b7e6c874ae18f67c6b
Pham Quoc Cuong <pzcuong@Pham-Quoc-Cuong.local>
2023-08-06 11:17:23 +0700

    fix history game bug

--- client/src/routes/(user)/(quiz)/dashboard/history/+page.svelte
+++ client/src/routes/(user)/(quiz)/dashboard/history/+page.svelte
@@ -1,47 +1,54 @@
 <script>
 	export let data;
-	console.log(data.result);
 	let games = data.result.data;
 </script>
 
 <div class="container mx-auto p-6">
 	<h1 class="text-4xl mb-6">Game History</h1>
 	<div class="overflow-x-auto">
-		<table class="w-full table-auto">
-			<thead>
-				<tr class="text-left">
-					<th class="px-4 py-2">Game</th>
-					<th class="px-4 py-2">Start date</th>
-					<th class="px-4 py-2">Complete date</th>
-					<th class="px-4 py-2">Score</th>
-					<th class="px-4 py-2">Status</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each games as game (game.id)}
-					<tr>
-						<td class="border px-4 py-2">{game.quiz.title}</td>
-						<td class="border px-4 py-2">{new Date(game.startedAt).toLocaleString('vi-VN', {
-							timeZone: 'Asia/Ho_Chi_Minh'
-						})}</td>
-						{#if game.completedAt == null}
-							<td class="border px-4 py-2">-</td>
-						{:else}
-							<td class="border px-4 py-2">{new Date(game.completedAt).toLocaleString('vi-VN', {
-								timeZone: 'Asia/Ho_Chi_Minh'
-							})}</td>
-						{/if}
-						<td class="border px-4 py-2">{game.point}</td>
-						<td class="border px-4 py-2">
-							{#if game.point >= game.quiz.passingPoint}
-								<span class="text-green-500">Passed</span>
+		{#if games.length == 0}
+			<p class="text-center">You have not played any game yet.</p>
+		{:else}
+			<table class="w-full table-auto">
+				<thead>
+					<tr class="text-left">
+						<th class="px-4 py-2">Game</th>
+						<th class="px-4 py-2">Start date</th>
+						<th class="px-4 py-2">Complete date</th>
+						<th class="px-4 py-2">Score</th>
+						<th class="px-4 py-2">Status</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each games as game}
+						<tr>
+							<td class="border px-4 py-2">{game.quiz.title}</td>
+							<td class="border px-4 py-2"
+								>{new Date(game.startedAt).toLocaleString('vi-VN', {
+									timeZone: 'Asia/Ho_Chi_Minh'
+								})}</td
+							>
+							{#if game.completedAt == null}
+								<td class="border px-4 py-2">-</td>
 							{:else}
-								<span class="text-red-500">Failed</span>
+								<td class="border px-4 py-2"
+									>{new Date(game.completedAt).toLocaleString('vi-VN', {
+										timeZone: 'Asia/Ho_Chi_Minh'
+									})}</td
+								>
 							{/if}
-						</td></tr
-					>
-				{/each}
-			</tbody>
-		</table>
+							<td class="border px-4 py-2">{game.point}</td>
+							<td class="border px-4 py-2">
+								{#if game.point >= game.quiz.passingPoint}
+									<span class="text-green-500">Passed</span>
+								{:else}
+									<span class="text-red-500">Failed</span>
+								{/if}
+							</td></tr
+						>
+					{/each}
+				</tbody>
+			</table>
+		{/if}
 	</div>
 </div>
