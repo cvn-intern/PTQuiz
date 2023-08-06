a9a804260f3fea15f0024a15cf5eb5959b5a73f7
Thanhtinh06 <105902029+Thanhtinh06@users.noreply.github.com>
2023-08-06 21:48:42 +0700

    style end-game battle

--- client/src/components/endGame/position.svelte
+++ client/src/components/endGame/position.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+	import Balloon from './balloon.svelte';
 
 	type Participant = {
 		id: string;
--- client/src/components/playGame/socket/settingsRoom.svelte
+++ client/src/components/playGame/socket/settingsRoom.svelte
@@ -98,7 +98,7 @@
 			<ImageModal bind:modalOpen={qrModalOpen} imageSrc={qrCode} />
 			<div class="flex flex-col justify-between w-full gap-4">
 				<div
-					class="flex md:flex-row-reverse items-center justify-between w-full h-12 gap-1"
+					class="flex flex-row-reverse items-center justify-between w-full h-12 gap-1"
 				>
 					<button
 						disabled={isCopied}
@@ -145,7 +145,7 @@
 						</div>
 					{/if}
 				</div>
-				<div class="flex md:flex-row-reverse justify-start items-center gap-1 h-12">
+				<div class="flex flex-row-reverse justify-start items-center gap-1 h-12">
 					<button
 						disabled={isChangedVisibility}
 						class={`p-2 bg-slate-50/30 shadow-xl rounded-lg h-full ${
