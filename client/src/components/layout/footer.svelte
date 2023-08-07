265a3425366c90f663238302e6ea68fe24ad0638
PandoraLH <lixhen2002@gmail.com>
2023-08-07 09:37:30 +0700

    change footer

--- client/src/components/layout/footer.svelte
+++ client/src/components/layout/footer.svelte
@@ -11,50 +11,76 @@
 	];
 </script>
 
-<footer class="bg-primary flex flex-col flex-end">
-	<div class="flex flex-col md:items-center">
-		<div
-			class="container flex flex-col items-start px-4 gap-8 md:flex-row md:justify-between lg:px-24 pt-4"
-		>
-			<div class="location flex flex-col items-start md:items-center gap-2">
-				<h1 class="text-xl font-bold" aria-label="">{$t('common.ourLocation')}</h1>
-				<div class="flex flex-col gap-2">
-					<span class="flex gap-2 items-center cursor-pointer">
-						<Icon icon="bxs:map" class="text-2xl" />
-						<div class="flex flex-col">
-							<p>{$t('common.address')}</p>
-							<p>{$t('common.city')}</p>
-						</div>
-					</span>
-					<span class="flex gap-2 items-center cursor-pointer">
-						<Icon icon="material-symbols:mail" class="text-2xl" />
-						<p class="">btdat1412@gmail.com</p>
-					</span>
-					<span class="flex gap-2 items-center cursor-pointer">
-						<Icon icon="ic:baseline-phone" class="text-2xl" />
-						<p class="">+84 12746060</p>
-					</span>
-				</div>
-			</div>
-			<div class="about flex flex-col items-start md:items-center gap-2">
-				<h1 class="text-xl font-bold">{$t('common.aboutUs')}</h1>
-				<ul class="flex flex-col gap-2">
-					<li class="cursor-pointer" aria-label="About us">{$t('common.aboutUs')}</li>
-					<li class="cursor-pointer" aria-label="Help">{$t('common.help')}</li>
-					<li class="cursor-pointer" aria-label="FAQS">{$t('common.faq')}</li>
-				</ul>
-			</div>
-			<div class="contact flex flex-col items-start md:items-center gap-2">
-				<h1 class="text-xl font-bold">{$t('common.contactUs')}</h1>
-				<div class="flex gap-2 cursor-pointer">
-					{#each contactIcons as icon}
-						<Icon {icon} class="text-2xl" />
-					{/each}
-				</div>
+<footer class="navbar bg-primary w-full px-4 lg:px-16 py-4 items-center">
+	<div class="flex flex-col w-full">
+		<div class="flex justify-between">
+			<div>DarkHorse Team</div>
+			<div class="flex gap-2 cursor-pointer">
+				{#each contactIcons as icon}
+					<Icon {icon} class="text-2xl" />
+				{/each}
 			</div>
 		</div>
-		<div class="copyright flex justify-center text-black font-semibold">
-			{$t('common.copyRights')}
+	</div>
+	<div class="">
+		<div class="container">
+			<div class="ak-bar-inner">
+				<div class="ak-row ak-row-items-middle ak-row-responsive">
+					<div class="ak-column ak-column-left ak-column-grow">
+						<div class="ak-inner-row ak-row-items-middle ak-justify-content-left">
+							<div class="ak-bar-item ak-footer-copyright">
+								{$t('common.copyRights')}
+							</div>
+						</div>
+					</div>
+					<div class="ak-column ak-column-right ak-column-grow">
+						<div class="ak-inner-row ak-row-items-middle ak-justify-content-right">
+							<div class="ak-bar-item ak-footer-menu-container">
+								<ul
+									id="menu-footer-navigation"
+									class="ak-menu ak-menu-wide ak-menu-style-2 ak-footer-menu"
+								>
+									<li
+										id="menu-item-547"
+										class="menu-item menu-item-type-custom menu-item-object-custom menu-item-547"
+									>
+										<a
+											href="https://themeforest.net/item/newsy-viral-news-magazine-wordpress-theme/34626838&amp;ref=akbilisim"
+											><span>Buy Now</span></a
+										>
+									</li>
+									<li
+										id="menu-item-548"
+										class="menu-item menu-item-type-custom menu-item-object-custom menu-item-548"
+									>
+										<a
+											href="https://support.akbilisim.com/docs/newsy/introduction"
+											><span>Documentation</span></a
+										>
+									</li>
+									<li
+										id="menu-item-549"
+										class="menu-item menu-item-type-custom menu-item-object-custom menu-item-549"
+									>
+										<a href="https://support.akbilisim.com/"
+											><span>Support Center</span></a
+										>
+									</li>
+									<li
+										id="menu-item-550"
+										class="menu-item menu-item-type-custom menu-item-object-custom menu-item-550"
+									>
+										<a
+											href="https://themeforest.net/user/akbilisim?ref=akbilisim"
+											><span>Contact Us</span></a
+										>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </footer>
